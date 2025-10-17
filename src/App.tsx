6e7a094e112c94392a7563a4422c0da5a2a6e695
Matheus Silva <ms25022003@gmail.com>
2025-10-17 10:08:25 -0300

    style(game): improve mobile style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon, CircleIcon, XIcon } from "lucide-react"
-import { Activity, useState } from "react"
+import { useState } from "react"
 
 const empty: Cell = {
   value: 0,
@@ -22,31 +22,31 @@ function App() {
       [empty, empty, empty, empty, empty],
       [empty, empty, empty, empty, empty]
   ]
-  const [matrix, setState] = useState(startGame)
+  const [matrix, setMatrix] = useState(startGame)
   const [hasWin, setWasWin] = useState(false)
   const [player, setPlayer] = useState<Player>("p1")
 
   function play(col: number) {
+    let lastEmptyRowIndex = -1
     for (let rowIndex = matrix.length - 1; rowIndex >= 0; rowIndex--) {
 
       if (matrix[rowIndex][col].value === 0) {
-        setState(prev => {
-          const updatedState = [...prev]
-          const updatedRow = updatedState[rowIndex]
-          updatedRow[col] = {
-            value: 1,
-            player
-          }
-          updatedState[rowIndex] = updatedRow
-          return updatedState
-        })
-        checkWinner(rowIndex, col)
+        lastEmptyRowIndex = rowIndex
         break
       }
     }
+    const updatedMatrix = matrix
+    const updatedRow = updatedMatrix[lastEmptyRowIndex]
+    updatedRow[col] = {
+      value: 1,
+      player
+    }
+
+    setMatrix(updatedMatrix)
+    checkWinner(lastEmptyRowIndex, col)
   }
 
-  function changePlayer() {
+  function switchPlayer() {
     setPlayer(prev => prev === "p1" ? "p2" : "p1")
   }
 
@@ -143,53 +143,50 @@ function App() {
 
         return assert
     })
+
     if (assert) {
-      setTimeout(() => {
-        setWasWin(true)
-      }, 800)
+      setWasWin(true)
     } else {
-      changePlayer()
+      switchPlayer()
     }
   }
 
   function reset() {
-    setState(startGame)
+    setMatrix(startGame)
     setWasWin(false)
     setPlayer("p1")
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <Activity mode={hasWin ? "visible" : "hidden"}>
-        <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50">
-          <section className="max-w-md w-full p-8 bg-white rounded-2xl flex flex-col gap-y-6 text-center border animate-fade-in">
-            <img src="https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/3x.webp" className="size-16 mx-auto" />
-            <div>
-              <h2 className="text-2xl font-bold text-gray-800 mb-2">Parabéns!</h2>
-              <p className="text-lg text-gray-600">
-                Jogador <span className="font-bold text-purple-600 uppercase">{player}</span> venceu!
-              </p>
-            </div>
-            <button 
-              onClick={reset} 
-              className="bg-purple-600 px-6 py-3 rounded-xl font-semibold hover:bg-purple-700 transition-all"
-            >
-              Jogar Novamente
-            </button>
-          </section>
-        </div>
-      </Activity>
-
+    <div className="min-h-screen flex items-center justify-center md:p-4">
       <section className="w-full max-w-xl">
         <div className="text-center mb-8">
-          <h1 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-orange-400 mb-2">
+          <h1 className="text-4xl md:text-5xl font-bold text-purple-400 mb-2">
             Ligue 3
           </h1>
           <p className="text-gray-400 text-sm">Conecte 3 peças para vencer!</p>
         </div>
 
-        <div className="bg-gradient-to-br from-gray-800 to-gray-900 rounded-3xl shadow-2xl p-8 border border-gray-700">
-          {!hasWin && (
+        <div className="bg-gray-900 rounded-3xl shadow-2xl p-2 md:p-8 border border-gray-700">
+          {hasWin ? (
+            <section className="flex flex-col text-center mb-8">
+              <div>
+                <div className="flex justify-center items-end">
+                  <img src="https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/3x.webp" className="size-8" />
+                  <h2 className="text-2xl font-bold leading-none">Parabéns!</h2>
+                </div>
+                <p className="text-lg">
+                  Jogador <span className={`font-bold uppercase ${player === "p1" ? "text-purple-600" : "text-orange-600"}`}>{player}</span> venceu!
+                </p>
+              </div>
+              <button 
+                onClick={() => reset()} 
+                className={`bg-purple-600 max-w-56 w-full mx-auto px-4 py-2 mt-2 rounded-xl font-semibold hover:bg-purple-700 transition-all`}
+              >
+                Jogar Novamente
+              </button>
+            </section>
+          ) : (
             <div className="mb-8 text-center">
               <div className="inline-flex items-center gap-3 bg-gray-700/50 px-6 py-3 rounded-full border border-gray-600">
                 <div className={`size-3 rounded-full ${player === "p1" ? "bg-purple-500" : "bg-orange-500"}`}></div>
@@ -199,49 +196,49 @@ function App() {
               </div>
             </div>
           )}
-
-          <div className="flex justify-between gap-3 mb-6 px-6 z-10 relative">
+          <section className="[--cell-size:calc(var(--spacing)*9)] md:[--cell-size:calc(var(--spacing)*12)]">
+          <div className="flex justify-between gap-3 mb-6 px-4 md:px-6 z-10 relative">
             {Array.from({length: 5}).map((_, i) => (
               <button 
                 disabled={hasWin} 
                 key={i} 
                 onClick={() => play(i)} 
-                className="group cursor-pointer rounded-full bg-purple-500 hover:bg-purple-400 size-12 p-2 disabled:opacity-30 disabled:cursor-not-allowed transition-all hover:shadow-purple-500/50 flex items-center justify-center"
+                className="group cursor-pointer rounded-full bg-purple-500 hover:bg-purple-400 size-(--cell-size) md:p-2 disabled:opacity-30 disabled:cursor-not-allowed transition-all hover:shadow-purple-500/50 flex items-center justify-center"
               >
                 <ChevronDownIcon className="size-full text-white group-hover:animate-bounce"/>
               </button>
             ))}
           </div>
 
-          <div className="bg-blue-900/40 rounded-2xl p-6 border-blue-800">
+          <div className="bg-blue-900/40 rounded-2xl py-6 px-4 md:px-6 border-blue-800">
             <div className="flex flex-col gap-3">
               {matrix.map((cells, i) => (
                 <div key={'row-'+i} className="flex justify-between gap-3">
                   {cells.map((cell, j) => (
                     <div 
                       key={'row-'+i+',col'+j} 
-                      className="rounded-full border-4 border-blue-700/60 bg-blue-950/60 flex items-center justify-center size-12 transition-all"
+                      className="rounded-full border-3 md:border-4 border-blue-700/60 bg-blue-950/60 flex items-center justify-center size-(--cell-size) transition-all"
                     >
                       {cell.value === 1 && (cell.player === "p1"
                         ? (
-                          <div className="animate-bounce-complex"
+                          <div className="animate-bounce-complex size-full"
                             style={{
                               // @ts-expect-error TS doesn't know this property
                               "--number-cells-before": i + 1,
                               animationDuration: `${0.3 + (i * 0.1)}s`
                             }}
                           >
-                            <XIcon className="size-10 text-purple-400 bg-purple-900/40 rounded-full p-1" strokeWidth={3} />
+                            <XIcon className="text-purple-400 bg-purple-900/40 rounded-full size-full p-1" strokeWidth={3} />
                           </div>
                         ) : (
-                          <div className="animate-bounce-complex"
+                          <div className="animate-bounce-complex size-full"
                             style={{
                               // @ts-expect-error TS doesn't know this property
                               "--number-cells-before": i + 1,
                               animationDuration: `${0.3 + (i * 0.1)}s`
                             }}
                           >
-                            <CircleIcon className="size-10 text-orange-400 bg-orange-900/40 rounded-full p-1" strokeWidth={3} />
+                            <CircleIcon className="text-orange-400 bg-orange-900/40 rounded-full size-full p-1" strokeWidth={3} />
                           </div>
                         ))}
                     </div>
@@ -250,6 +247,7 @@ function App() {
               ))}
             </div>
           </div>
+          </section>
 
           <div className="flex justify-center gap-8 mt-6">
             <div className="flex items-center gap-2">
